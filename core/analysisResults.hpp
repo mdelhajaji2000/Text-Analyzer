0dbab4151fd924dd78a586288b121d9d900b8d39
mdelhajaji2000 <149912377+mdelhajaji2000@users.noreply.github.com>
2026-01-29 21:38:53 +0100

    Add analysisResults and tokenizerResults structures for analysis data storage

---
+++ core/analysisResults.hpp
@@ -0,0 +1,13 @@
+#include <string>
+
+#include "..\containers\sList.h"
+#include "..\containers\dynamic_array.h"
+
+struct analysisResults
+{
+    std::string frequent_word;
+    char frequent_char;
+    std::size_t word_count;
+    std::size_t char_count;
+};
+
---
+++ core/analyzer.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "tokenizer.hpp"
+#include "..\containers\dynamic_array.h"
+#include "..\containers\sList.h"
+class analyzer
+{
+private:
+    
+public:
+    
+};
